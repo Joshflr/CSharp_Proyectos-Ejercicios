85199e2004563a1314b34e26c89c367dceb7a986
Josue Flores <3005169400101@ingenieria.usac.edu.gt>
2022-09-16 16:38:37 -0600

    Proyectos&Ejercicios

---
+++ Ejercicios_de_Ciclos/Numerosal1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Numerosal1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
